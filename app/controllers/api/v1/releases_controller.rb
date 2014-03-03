784c57f600654b512ec146cf85e2fb85c5aedba7
Simon Croome <simon@croome.org>
2014-03-03 00:29:38 +0000

    Refactor releases, move table from postgres to couch

--- app/controllers/api/v1/releases_controller.rb
+++ app/controllers/api/v1/releases_controller.rb
@@ -5,28 +5,33 @@ class Api::V1::ReleasesController < ApplicationController
   # GET /releases
   # GET /releases.json
   def index
-    render json: Release.includes(:product).load.as_json(:include => :product)
+    render json: Release.all
   end
 
   def show
-    render json: @release.as_json(:include => :product)
+    render json: @release
+  end
+
+  def refresh
+    Resque.enqueue(AddReleases)
+    render json: {}
   end
 
   # PUT /releases/1/check
   def check
-    Resque.enqueue(Mcollective::CheckRelease, @release.id)
+    Resque.enqueue(Mcollective::CheckRelease, @release.slug)
     render json: {}
-  end  
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_release
-      @release = Release.includes(:product).find(params[:id])
+      @release = Release.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def release_params
-      params.require(:release).permit(:version, :product_id, :status)
+      params.require(:release).permit(:slug, :status)
     end
 
 end
