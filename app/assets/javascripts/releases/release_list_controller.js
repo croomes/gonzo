46083c1aa344e0507baefb84cc7bde6d1d16fb5d
Simon Croome <simon@croome.org>
2014-03-10 00:34:18 +0000

    Add deployment tracking summary to releases index

--- app/assets/javascripts/releases/release_list_controller.js
+++ app/assets/javascripts/releases/release_list_controller.js
@@ -1,3 +1,17 @@
 gonzo.controller('ReleaseListCtrl', ['$scope', 'Restangular', function($scope, Restangular) {
   $scope.releases = Restangular.all("releases.json").getList().$object;
+  $scope.deployment = {};
+
+  $scope.getDeploymentData = function() {
+    Restangular.oneUrl('nodes', 'http://localhost:5984/mcollective/_design/releasedeployment/_view/all?reduce=true&group=true').get().then(function(res) {
+      res.rows.forEach(function(row) {
+        $scope.deployment[row['key']] = row['value'];
+      });
+    }, function(reason) {
+      console.log(reason);
+    });
+  };
+
+  $scope.getDeploymentData();
+
 }]);
