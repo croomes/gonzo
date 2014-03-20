af9130df9342ba5d0061a2d30d8a920ecc15d927
Simon Croome <simon@croome.org>
2014-03-20 09:28:07 +0000

    Release summary graphs working

--- app/assets/javascripts/releases/release_list_controller.js
+++ app/assets/javascripts/releases/release_list_controller.js
@@ -9,12 +9,22 @@ gonzo.controller('ReleaseListCtrl', ['$scope', '$stateParams', 'Restangular', fu
   $scope.getDeploymentData = function() {
     Restangular.oneUrl('nodes', 'http://localhost:5984/mcollective/_design/releasedeployment/_view/all?reduce=true&group=true').get().then(function(res) {
       res.rows.forEach(function(row) {
+        total = 0;
         $scope.deployment[row['key']] = row['value'];
+        for(var tier in row['value']) {
+          total += row['value'][tier];
+        }
+        $scope.deployment[row['key']]['total'] = total;
       });
+
     }, function(reason) {
       console.log(reason);
     });
   };
+
+  $scope.getReleaseMaxNodes = function(release) {
+    return $scope.deployment[release] ? $scope.deployment[release]['total'] : 0;
+  }
 
   $scope.row_class = function(version) {
     if (version === $scope.version) {
